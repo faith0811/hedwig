541c3cb654adc49b52f2d3d929b4c03f0012745f
Wang Yanqing <yanqing.wang@ele.me>
2015-04-30 10:50:18 +0800

    build skeleton of restful api

--- app.js
+++ app.js
@@ -3,17 +3,20 @@
  * Main Application
  */
 
+"use strict";
+
 /**
  * Module dependencies
  */
 
-"use strict";
-
 var express = require('express'),
-  router = require('./router'),
-  http = require('http'),
-  path = require('path'),
-  server_settings = require('./config').SERVER_SETTINGS
+    bodyParser = require('body-parser'),
+    routers = require('./router'),
+    apis = require('./router/api'),
+    sockets = require('./router/socket'),
+    http = require('http'),
+    path = require('path'),
+    server_settings = require('./config').SERVER_SETTINGS;
 
 
 var app = module.exports = express();
@@ -25,6 +28,8 @@ var io = require('socket.io').listen(server);
  */
 
 app.set('port', server_settings.port || 7201);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 if (app.get('env') === 'dev') {
   // TODO DEVELOP ENVIRONMENT
@@ -38,14 +43,25 @@ if (app.get('env') === 'prod') {
  * Socket Register
  */
 
-io.sockets.on('connection', require('./router/socket'));
+io.sockets.on('connection', sockets);
 
 /**
  * API Router
- * Will Not Be Used Now
  */
 
-// app.get('/', router);
+app.get('/api', apis.index);
+app.get('/api/ping', apis.ping);
+
+app.get('/api/message', apis.get_message_info);
+app.post('/api/message', apis.new_message);
+
+app.get('/api/message/:message_id', apis.get_message);
+
+app.get('/api/user', apis.get_user_info);
+app.post('/api/user', apis.new_user);
+app.delete('/api/user', apis.delete_user);
+
+app.get('/api/user/:user_id', apis.get_user);
 
 /**
  * Start Server
@@ -53,4 +69,4 @@ io.sockets.on('connection', require('./router/socket'));
 
 server.listen(app.get('port'), function () {
   console.log('Server Listening on port: ' + app.get('port'));
-})
+});
---
+++ router/api.js
@@ -0,0 +1,42 @@
+
+/**
+ * RESTful API
+ */
+
+"use strict";
+
+exports.index = function (req, res) {
+
+};
+
+exports.ping = function (req, res) {
+  res.json('ping');
+};
+
+exports.get_message_info = function (req, res) {
+
+};
+
+exports.new_message = function (req, res) {
+
+};
+
+exports.get_message = function (req, res) {
+
+};
+
+exports.get_user_info = function (req, res) {
+
+};
+
+exports.new_user = function (req, res) {
+
+};
+
+exports.delete_user = function (req, res) {
+
+};
+
+exports.get_user = function (req, res) {
+
+};
